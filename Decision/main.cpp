1835b2af7852fd41772869656d7b61376d543392
Dkuran <kamelotd13@hotmail.com>
2014-10-29 16:15:57 -0500

    Initial Commit

---
+++ Decision/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Decision
+//
+//  Created by Andres Duran on 29/10/14.
+//  Copyright (c) 2014 Andres Duran. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
